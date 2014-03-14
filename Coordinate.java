0b9dadc9e8c92fa0738ac016fedac2b507d1b6f7
Chan Park <chanheepark807@gmail.com>
2014-03-14 16:49:00 -0700

    changed

---
+++ Coordinate.java
@@ -0,0 +1,13 @@
+/**
+ *	Coordinate class represents the location of a piece on the board.
+ *	No invariants.
+ */
+public class Coordinate {
+	public int x;
+	public int y;
+
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
